b95e8635913bb55d8309764f5550cfb0e7b2734f
Julian Tellez <4896851+juliantellez@users.noreply.github.com>
2020-11-29 21:28:09 +0000

    chore: bootsrap npx typescript-react (#1)

---
+++ jest.config.js
@@ -0,0 +1,42 @@
+const filExtensions =
+    '\\.(css|scss|sass|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$'
+
+const config = {
+    moduleNameMapper: {
+        [filExtensions]: 'identity-obj-proxy',
+    },
+    /**
+     * Watch
+     */
+    watchPlugins: [
+        "jest-watch-typeahead/filename",
+        "jest-watch-typeahead/testname"
+    ],
+    /**
+     * NYC coverage
+     * see https://jestjs.io/docs/en/configuration#coveragedirectory-string
+     */
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx,js,jsx}',
+        /**
+         * Exclude style declaration files
+         */
+        '!src/**/*.{scss,css,scss}.d.ts',
+        /**
+         * Exclude storybook files
+         */
+        '!src/**/*/*.stories.{ts,tsx,js,jsx}',
+    ],
+    coverageReporters: ['lcov', 'text', 'text-summary'],
+    coverageThreshold: {
+        global: {
+            branches: 98,
+            functions: 98,
+            lines: 98,
+            statements: 98,
+        },
+    },
+}
+
+module.exports = config
