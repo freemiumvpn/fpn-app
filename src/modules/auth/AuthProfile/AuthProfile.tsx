af8bd3993d25ede2c977cfb2268c3058bbe94961
Julian <juliantellezmendez@gmail.com>
2020-12-01 00:15:59 +0000

    chore: add home and login pages;

--- src/modules/auth/AuthProfile/AuthProfile.tsx
+++ src/modules/auth/AuthProfile/AuthProfile.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 
+import AuthLogout from '../AuthLogout'
+
 const AuthProfile: React.FC = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0()
-
-  if (isLoading) {
-    return <div>Loading ...</div>
-  }
-
-  if (!isAuthenticated) {
-    return null
-  }
+  const { user } = useAuth0()
 
   return (
     <div>
+      <AuthLogout />
       <img src={user.picture} alt={user.name} />
       <h2>{user.name}</h2>
       <p>{user.email}</p>
