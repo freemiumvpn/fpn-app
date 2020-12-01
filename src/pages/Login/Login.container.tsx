3ec7fa707cf3f8f8f995b5ac92f8ac709e0f80ea
Julian Tellez <4896851+juliantellez@users.noreply.github.com>
2020-12-01 01:09:21 +0000

    chore: add login page content + styles (#7)

--- src/pages/Login/Login.container.tsx
+++ src/pages/Login/Login.container.tsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 import AuthLogin from '../../modules/auth/AuthLogin'
 
+import styles from './Login.scss'
+
 const LoginPage: React.FC = () => {
-  return <AuthLogin />
+  return (
+    <main className={styles.main}>
+      <section className={styles.box}>
+        <header className={styles.header}>
+          <img
+            className={styles.img}
+            id="prompt-logo-center"
+            src="https://raw.githubusercontent.com/fremiumvpn/fpn-app/main/assets/img/fpn.png?raw=true"
+            alt="Welcome"
+          />
+          <h1 className={styles.title}>Welcome</h1>
+        </header>
+
+        <div className={styles.body}>
+          <div className={styles.text}>It&apos; going to be great!</div>
+          <AuthLogin className={styles.button} />
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default LoginPage
