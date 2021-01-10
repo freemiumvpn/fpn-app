8988083d582a188484ae6c0428afd849827c30c1
Julian <juliantellezmendez@gmail.com>
2021-01-10 08:17:01 +0000

    chore: remove unused code

--- src/Example/Example.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-
-import Example from './Example'
-
-storiesOf('Example Group', module).add('Example Story', () => <Example />)
