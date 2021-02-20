00e51fd001f3f98d0d57cdc932d41a30f37daf40
Julian <juliantellezmendez@gmail.com>
2021-02-20 22:00:25 +0000

    fix: lint

--- src/pages/Welcome/components/WelcomeInstall/WelcomeInstall.tsx
+++ src/pages/Welcome/components/WelcomeInstall/WelcomeInstall.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Button from '@material-ui/core/Button'
 import Link from '@material-ui/core/Link'
 
 import { AppStore } from '../../../../modules/icon/AppStore/AppStore'
