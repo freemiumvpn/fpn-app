6ab82bb45c06ac50510dfbcda694fe15ee70d356
Julian Tellez <4896851+juliantellez@users.noreply.github.com>
2021-02-20 11:35:01 +0000

    fix(login): redirect if already logged in (#27)

--- src/pages/Login/Login.container.tsx
+++ src/pages/Login/Login.container.tsx
@@ -1,10 +1,24 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
 import AuthLogin from '../../modules/auth/AuthLogin'
+import { Path } from '../../routes/routes'
+import SplashPage from '../Splash/Splash'
 
 import styles from './Login.scss'
 
 const LoginPage: React.FC = () => {
+  const { isAuthenticated, isLoading } = useAuth0()
+
+  if (isLoading) {
+    return <SplashPage />
+  }
+
+  if (isAuthenticated) {
+    return <Redirect to={Path.USER} />
+  }
+
   return (
     <main className={styles.main}>
       <section className={styles.box}>
