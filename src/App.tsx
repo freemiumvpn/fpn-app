130cb3720380c353233ecb109dd4de6516abdfbb
Julian <juliantellezmendez@gmail.com>
2021-02-27 14:48:10 +0000

    chore(auth): login and signup redirect

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,14 @@ import { ApolloProvider } from '@apollo/client'
 import './shared/styles/global.scss'
 import Routes from './routes/routes'
 import SplashPage from './pages/Splash/Splash'
-import AuthProvider from './modules/auth/AuthProvider'
+import { AuthProvider } from './modules/auth/AuthProvider'
 import { ContextProvider } from './context/Context'
 import gqlClient from './middlewares/gql/client'
-import { getEnvVars } from './env'
 
 const App: React.FC = () => {
-  const env = getEnvVars()
-
   return (
     <React.Suspense fallback={<SplashPage />}>
-      <AuthProvider
-        domain={env.auth0.domain}
-        clientId={env.auth0.clientId}
-        redirectUri={env.auth0.redirectUri}
-        audience={env.auth0.audience}
-        scope={env.auth0.scope}
-      >
+      <AuthProvider>
         <ContextProvider>
           <ApolloProvider client={gqlClient}>
             <Router>
--- src/modules/auth/AuthProvider/Auth.provider.tsx
+++ src/modules/auth/AuthProvider/Auth.provider.tsx
@@ -1,3 +1,22 @@
+import React from 'react'
 import { Auth0Provider } from '@auth0/auth0-react'
 
-export default Auth0Provider
+import { getEnvVars } from '../../../env'
+
+const env = getEnvVars()
+
+const AuthProvider: React.FC = props => {
+  return (
+    <Auth0Provider
+      domain={env.auth0.domain}
+      clientId={env.auth0.clientId}
+      audience={env.auth0.audience}
+      scope={env.auth0.scope}
+      redirectUri={env.auth0.redirectUri}
+    >
+      {props.children}
+    </Auth0Provider>
+  )
+}
+
+export { AuthProvider }
