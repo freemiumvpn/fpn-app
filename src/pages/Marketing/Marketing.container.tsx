1dcce3d88525c79c583368ea0b207d8ba2dbae75
Julian <juliantellezmendez@gmail.com>
2021-04-02 22:35:20 +0100

    chore(pages): improve twitter card

--- src/pages/Marketing/Marketing.container.tsx
+++ src/pages/Marketing/Marketing.container.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import { AppContext } from '../../context/Context'
+import { AnalyticsEventType } from '../../middlewares/analytics/Analytics'
+import { Path } from '../../routes/routes'
+
 import { MarketingBasePage } from './Marketing.base'
 
 const MarketingPage: React.FC = () => {
+  const {
+    analytics: { analytics$ },
+  } = React.useContext(AppContext)
+
+  React.useEffect(() => {
+    analytics$.next({
+      event: AnalyticsEventType.APP_PAGE_LOAD,
+      data: Path.MARKETING_IOS_BETA,
+    })
+  }, [analytics$])
+
   return <MarketingBasePage />
 }
 
